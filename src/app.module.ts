bdc0e3bdf0a02dc901ad546cf7bc29c53d0f08ba
Esteban <juanescalderon12@gmail.com>
2022-12-03 15:25:55 -0500

    Configurando algunas variables de entorno y inicializando la conexión a PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ ],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // El + es porque debe ser un número.
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+  ],
   controllers: [ ],
   providers: [ ],
 })
