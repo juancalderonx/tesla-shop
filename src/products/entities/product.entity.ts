a7069a2da0fe030614974414cb1080fa460c006b
juancalderonx <juanescalderon12@gmail.com>
2022-12-04 20:36:55 -0500

    Create products v1

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -6,9 +6,49 @@ export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: number;
 
+
   @Column('text', {
     unique: true
   })
   title: string;
+
+
+  @Column('float', {
+    default: 0
+  })
+  price: number;
+
+
+  @Column({
+    type: "text",
+    nullable: true
+  })
+  description: string;
+
+
+  @Column({
+    unique: true
+  })
+  slug: string;
+
+
+  @Column('int', {
+    default: 0
+  })
+  stock: number;
+
+
+  @Column('text', {
+    array: true
+  })
+  sizes: string[];
+
+
+  @Column('text')
+  gender: string;
+
+
+  // Tags and Images
+
 
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common/exceptions';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    
+    try {
+
+      // Creo el producto en base a los datos que llegaron del Frontend.
+      const product = this.productRepository.create(createProductDto);
+
+      // Guardo el producto en DB.
+      await this.productRepository.save(product);
+
+      return product;
+
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException('Error en el servidor, ayuda.')
+    }
+
   }
 
   findAll() {
