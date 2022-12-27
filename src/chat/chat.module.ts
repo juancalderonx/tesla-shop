767c740d8d07712d826c91a3d438c0fbf8f3401e
Esteban <juanescalderon12@gmail.com>
2022-12-27 15:09:50 -0500

    feat(chat): :speech_balloon: Finish chat websockets module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  providers: [ ChatGateway, ChatService ],
+  imports: [ AuthModule ]
 })
 export class ChatModule {}
